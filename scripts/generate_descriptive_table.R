4f42a0992d3df5f0dd9f438178e5395ca2a7698f
hoshidesu <aaronjsisonbusiness@gmail.com>
2026-07-14 23:24:20 +0900

    chore: clean up intermediate files, fix latex table script

--- scripts/generate_descriptive_table.R
+++ scripts/generate_descriptive_table.R
@@ -1,64 +1,80 @@
 library(tidyverse, warn.conflicts = FALSE)
 
-# Read descriptive statistics from pipeline output
 desc <- read_csv("data/processed/descriptive_stats.csv", show_col_types = FALSE)
 
-colnames(desc) <- c("Variable", "Stat", "Paper", "Extended", "Full")
+desc_wide <- desc |>
+  pivot_longer(c(N, Mean, SD, Min, Max), names_to = "stat", values_to = "value") |>
+  pivot_wider(names_from = period, values_from = value)
 
-# Generate LaTeX rows
-rows <- desc |>
-  mutate(Variable = str_replace_all(Variable, "_", "\\\\_")) |>
-  group_by(Variable) |>
-  mutate(row_number = row_number()) |>
+pn <- setdiff(names(desc_wide), c("variable", "stat"))
+
+full_sample <- desc |>
+  pivot_longer(c(N, Mean, SD, Min, Max), names_to = "stat", values_to = "value") |>
+  group_by(variable, stat) |>
+  summarise(full = mean(value, na.rm = TRUE), .groups = "drop")
+
+rows <- desc_wide |>
+  left_join(full_sample, by = c("variable", "stat")) |>
+  mutate(variable = str_replace_all(variable, "_", "\\\\_")) |>
+  group_by(variable) |>
+  mutate(first = row_number() == 1) |>
   ungroup() |>
+  rowwise() |>
   mutate(
-    display_var = ifelse(row_number == 1, Variable, ""),
-    paper  = ifelse(Stat == "N", sprintf("%s", Paper), sprintf("%8.4f", Paper)),
-    ext    = ifelse(Stat == "N", sprintf("%s", Extended), sprintf("%8.4f", Extended)),
-    full   = ifelse(Stat == "N", sprintf("%s", Full), sprintf("%8.4f", Full))
-  )
+    c1 = ifelse(stat == "N", as.character(round(.data[[pn[1]]], 0)), sprintf("%8.4f", .data[[pn[1]]])),
+    c2 = ifelse(stat == "N", as.character(round(.data[[pn[2]]], 0)), sprintf("%8.4f", .data[[pn[2]]])),
+    c3 = ifelse(stat == "N", as.character(round(full, 0)), sprintf("%8.4f", full))
+  ) |>
+  ungroup() |>
+  mutate(line = paste0(
+    ifelse(first & row_number() > 1, "\\addlinespace\n", ""),
+    ifelse(first, sprintf("%-15s", variable), "               "),
+    " & ", sprintf("%-5s", stat),
+    " & ", sprintf("%10s", c1),
+    " & ", sprintf("%10s", c2),
+    " & ", sprintf("%10s", c3), "\\\\"
+  ))
 
-# Write LaTeX file
-cat("\\documentclass[10pt]{article}
-\\usepackage[utf8]{inputenc}
-\\usepackage[margin=0.3in]{geometry}
-\\usepackage{booktabs}
-\\usepackage{longtable}
+bs <- "\\"
+preamble <- c(
+  paste0(bs, "documentclass[10pt]{article}"),
+  paste0(bs, "usepackage[utf8]{inputenc}"),
+  paste0(bs, "usepackage[margin=0.3in]{geometry}"),
+  paste0(bs, "usepackage{booktabs}"),
+  paste0(bs, "usepackage{longtable}"),
+  "",
+  paste0(bs, "begin{document}"),
+  paste0(bs, "pagestyle{empty}"),
+  paste0(bs, "setlength{", bs, "tabcolsep}{4pt}"),
+  paste0(bs, "renewcommand{", bs, "arraystretch}{0.85}"),
+  "",
+  paste0(bs, "begin{longtable}{l l r r r}"),
+  paste0("  ", bs, "caption*{Descriptive Statistics by Period} ", bs, "\\\\"),
+  paste0("  ", bs, "toprule"),
+  paste0("  ", bs, "textbf{Variable} & ", bs, "textbf{Stat} & ",
+    bs, "textbf{", pn[1], "} & ", bs, "textbf{", pn[2], "} & ",
+    bs, "textbf{Full Sample} ", bs, "\\\\"),
+  paste0("  ", bs, "midrule"),
+  paste0("  ", bs, "endfirsthead"),
+  paste0("  ", bs, "toprule"),
+  paste0("  ", bs, "textbf{Variable} & ", bs, "textbf{Stat} & ",
+    bs, "textbf{", pn[1], "} & ", bs, "textbf{", pn[2], "} & ",
+    bs, "textbf{Full Sample} ", bs, "\\\\"),
+  paste0("  ", bs, "midrule"),
+  paste0("  ", bs, "endhead"),
+  paste0("  ", bs, "bottomrule"),
+  paste0("  ", bs, "endfoot"),
+  paste0("  ", bs, "bottomrule"),
+  paste0("  ", bs, "endlastfoot")
+)
 
-\\begin{document}
-\\pagestyle{empty}
-\\setlength{\\tabcolsep}{4pt}
-\\renewcommand{\\arraystretch}{0.85}
+closing <- c(
+  paste0(bs, "end{longtable}"),
+  "",
+  paste0(bs, "end{document}")
+)
 
-\\begin{longtable}{l l r r r}
-\\caption*{Descriptive Statistics by Period} \\\\
-\\toprule
-\\textbf{Variable} & \\textbf{Stat} & \\textbf{1999--2021 (Paper)} & \\textbf{2021--2026 (Extended)} & \\textbf{Full Sample} \\\\
-\\midrule
-\\endfirsthead
-\\toprule
-\\textbf{Variable} & \\textbf{Stat} & \\textbf{1999--2021 (Paper)} & \\textbf{2021--2026 (Extended)} & \\textbf{Full Sample} \\\\
-\\midrule
-\\endhead
-\\bottomrule
-\\endfoot
-\\bottomrule
-\\endlastfoot
-", file = "drafts/descriptive_stats_table.tex")
-
-# Write each row group
-current_var <- ""
-for (i in seq_len(nrow(rows))) {
-  r <- rows[i, ]
-  sep <- ifelse(r$row_number == 1 && i > 1, "\\addlinespace\n", "")
-  line <- sprintf("%s%-15s & %-5s & %10s & %10s & %10s \\\\\n",
-    sep, r$display_var, r$Stat, r$paper, r$ext, r$full)
-  cat(line, file = "drafts/descriptive_stats_table.tex", append = TRUE)
-}
-
-cat("\\end{longtable}
-
-\\end{document}
-", file = "drafts/descriptive_stats_table.tex", append = TRUE)
-
-cat("LaTeX table written to drafts/descriptive_stats_table.tex\n")
+dir.create("drafts", showWarnings = FALSE)
+tex_path <- file.path("drafts", "descriptive_stats_table.tex")
+writeLines(c(preamble, rows$line, closing), tex_path)
+cat("LaTeX table written to", tex_path, "\n")
